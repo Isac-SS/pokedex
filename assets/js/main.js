1a6dc79c1eda9e98afc691bf2098469e785d869d
Isac-SS <isacsants679@gmail.com>
2022-12-14 12:55:37 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+function convertPokemonToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class"type">${typeSlot.type.name}</li>`)
+    }
+
+
+function convertPokemonToLi (pokemon) {
+    return `
+    <li class="pokemon">
+                <span class="number">#${pokemon.order}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail"> 
+                    <ol class="types">
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
